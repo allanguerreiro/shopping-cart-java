ed9b3db16b89febde352c36633d93ae132062433
allancarneiro <allan.carneiro@gft.com>
2019-10-13 12:33:27 -0300

    ADD: More Tests

--- shopping-cart-java/src/test/java/br/com/altran/carrinho/controller/CartControllerIntegrationTest.java
+++ shopping-cart-java/src/test/java/br/com/altran/carrinho/controller/CartControllerIntegrationTest.java
@@ -1,7 +1,9 @@
 package br.com.altran.carrinho.controller;
 
 import br.com.altran.carrinho.Application;
+import br.com.altran.carrinho.model.Cart;
 import br.com.altran.carrinho.model.Customer;
+import br.com.altran.carrinho.model.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -33,51 +35,28 @@ public class CartControllerIntegrationTest {
     }
 
     @Test
-    public void testGetAllEmployees() {
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/api/v1/customer/customers", HttpMethod.GET, entity, String.class);
-
-        assertNotNull(response.getBody());
-    }
-
-    @Test
-    public void testGetCustomerByName() {
-        Customer customer = restTemplate.getForObject(getRootUrl() + "/api/v1/customer/customerbyname/Allan", Customer.class);
-        log.debug(customer.getId());
-        assertNotNull(customer);
-    }
-
-    @Test
     public void testCreateCustomer() {
-        Customer customer = new Customer();
-        customer.setEmail("admin@gmail.com");
-        customer.setName("Admin");
+        Customer customer = new Customer(null, "Guerreiro", "allancarneiro@gmail.com");
+        ResponseEntity<Customer> customerResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/customer/create", customer, Customer.class);
+        assertNotNull(customerResponse);
+        assertNotNull(customerResponse.getBody());
 
-        ResponseEntity<Customer> postResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/customer/create", customer, Customer.class);
-        assertNotNull(postResponse);
-        assertNotNull(postResponse.getBody());
-    }
-
-    @Test
-    public void testUpdateCustomer() {
-        Customer customer = new Customer();
-        customer.setEmail("admin1@gmail.com");
-        customer.setName("Admin");
-
-        restTemplate.put(getRootUrl() + "/api/v1/customer/update", customer);
-
-        Customer updatedCustomer = restTemplate.getForObject(getRootUrl() + "/api/v1/customer/customerbyname/Admin", Customer.class);
-        assertNotNull(updatedCustomer);
-    }
-
-    @Test
-    public void testDeleteEmployee() {
-        this.testCreateCustomer();
-        Customer customer = restTemplate.getForObject(getRootUrl() + "/api/v1/customer/customerbyname/Admin", Customer.class);
+        customer = restTemplate.getForObject(getRootUrl() + "/api/v1/customer/customerbyname/Guerreiro", Customer.class);
         assertNotNull(customer);
 
-        restTemplate.delete(getRootUrl() + "/api/v1/customer/delete/" + customer.getId());
+        Item itemA = restTemplate.getForObject(getRootUrl() + "/api/v1/item/itembyname/Celular", Item.class);
+        assertNotNull(itemA);
+
+        Item itemB = restTemplate.getForObject(getRootUrl() + "/api/v1/item/itembyname/Apontador", Item.class);
+        assertNotNull(itemB);
+
+        List<Item> items = new ArrayList<>();
+        items.add(itemA);
+        items.add(itemB);
+
+        Cart cart = new Cart(null, customer.getId(), items);
+        ResponseEntity<Cart> cartResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/cat/create", cart, Cart.class);
+        assertNotNull(cartResponse);
+        assertNotNull(cartResponse.getBody());
     }
 }
